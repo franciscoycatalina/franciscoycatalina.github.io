616eaa6a93cfa3e8c9666d6944fb715f8773954c
franciscoycatalina <134017782+franciscoycatalina@users.noreply.github.com>
2023-05-18 23:07:07 -0400

    Add files via upload

---
+++ js/varios.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", function() {
+    var loader = document.getElementById("loader");
+    var contenido = document.getElementById("contenido");
+
+    loader.style.display = "none";
+    contenido.style.display = "block";
+});
+
+var parametrosURL = new URLSearchParams(window.location.search);
+var valor = parametrosURL.get("invitado");
+$(".name-invited").fadeOut(1000, function() {
+    $(this).html("<br>Y nos encantaría que nos acompañaras<br>" + valor).fadeIn(1000);
+  });
