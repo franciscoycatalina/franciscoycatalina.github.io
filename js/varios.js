7682d400c8b1b38354c66597e9ca2c4ff4d11a28
franciscoycatalina <134017782+franciscoycatalina@users.noreply.github.com>
2023-05-20 17:10:58 -0400

    Add files via upload

--- js/varios.js
+++ js/varios.js
@@ -6,12 +6,19 @@ window.addEventListener("load", function() {
     contenido.style.display = "block";
 });
 
+$(".nsc").fadeOut(2000, function() {
+    $(this).html("¡Nos casamos!").fadeIn(1000);
+});
+
 var parametrosURL = new URLSearchParams(window.location.search);
-var valor = parametrosURL.get("invitado");
+
+var valor = atob(parametrosURL.get("invitado"));
 
 if(valor){
-    $(".name-invited").fadeOut(2000, function() {
-        $(this).html("<br>Y nos encantaría que nos acompañaras<br><strong>" + valor+"</strong>").fadeIn(900);
+    $(".name-invited").fadeOut(4500, function() {
+        $(this).html("<br>Y nos encantaría que nos acompañaras<br><strong>" + valor+"</strong> el próximo 30 de septiembre").fadeIn(2500);
     });
+
+    $(".only-name").html("<strong>" +valor.charAt(0).toUpperCase() + valor.slice(1)+"</strong>");
 }
 
